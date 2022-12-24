49af52758400ac42cad06119d52b05cda631e7f0
Manot Luijiu <manot.luijiu@gmail.com>
2022-12-24 18:09:06 +0700

    adding github action

--- apps/next-app/src/components/HelloWorld.tsx
+++ apps/next-app/src/components/HelloWorld.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Button } from 'ui';
 
+
 const HelloWorld = () => (
   <>
-    <h1>HelloWorld</h1>
+    <h1>Hello Manot L.</h1>
     <label htmlFor="name">
       <input type="text" name="name" id="name" />
     </label>
--- apps/server/src/lib/add.test.ts
+++ apps/server/src/lib/add.test.ts
@@ -1,7 +1,8 @@
 import add from './add';
 
+
 describe('testing add', () => {
-  it('should be 3', () => {
-    expect(add(1, 2)).toBe(3);
+  it('should be 4', () => {
+    expect(add(1, 3)).toBe(4);
   });
 });
